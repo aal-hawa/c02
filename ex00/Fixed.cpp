c92455dfebca6f043461a6e136881cf05be8d8ec
Anas Al Hawamda <aal-hawa@lab1r3s7.42abudhabi.ae>
2025-06-01 16:56:54 +0400

    edit

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -3,6 +3,7 @@
 Fixed::Fixed()
 {
 	std::cout << "Default constructor called" << std::endl;
+	_rawBits = 0;
 }
 
 Fixed::~Fixed()
@@ -12,13 +13,29 @@ Fixed::~Fixed()
 
 Fixed::Fixed(const Fixed& other)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	_rawBits = other.getRawBits();
 }
 
 Fixed& Fixed::operator=(const Fixed& other)
 {	
+	std::cout << "Copy assignment operator called" << std::endl;
+
 	if (this != &other)
 		_rawBits = other.getRawBits();
 	return *this;
 }
 
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+
+	return _rawBits;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+
+	_rawBits = raw;
+}
